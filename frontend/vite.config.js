35bffcbf661555011d549edcef33914d896032ad
Omar Sanad <omarsanad.me@gmail.com>
2025-04-16 20:48:57 +0200

    for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,26 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8080",
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//     port: 3000,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Use relative paths for assets
   server: {
     proxy: {
       "/api": {
